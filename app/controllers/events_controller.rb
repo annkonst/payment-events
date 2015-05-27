aa5a6c4194f7b6a7f88a4b2f2ca57c58038dd119
Ann Gunina <gunina.a@list.ru>
2015-05-27 18:13:52 +0300

    list of events(edit,delete)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,43 @@
 class EventsController < ApplicationController
   before_filter :authenticate_user!
+
+  def index
+    @events = Event.all
+  end
+
   def show
     @user = current_user
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save!
+    redirect_to :index
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if event_params.present?
+      @event.update(event_params)
+    end
+      redirect_to events_path
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :date)
   end
 end
