16c5a803bb1364f67a90ff53bfd986395f15e5a1
SidorenkoD <frg-lost@mail.ru>
2016-02-29 14:14:51 +0300

    Added s3/Remarks

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,15 @@ class User < ActiveRecord::Base
   has_many :events
   has_many :invites, dependent: :destroy
   has_and_belongs_to_many :product_lists
-  has_attached_file :avatar, styles: { thumb: "100x100>" }
+  has_attached_file :avatar,
+                    styles: { thumb: "100x100!" },
+                    storage: :s3,
+                    s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
+                                      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] }
   validates :name, :phone_number, presence: true
   validates_attachment :avatar, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
-                                size: { in: 0..300.kilobytes, message: I18n.t(:less_than_300) }
+                                size: { less_than: 300.kilobytes, message: I18n.t(:less_than_300) }
   validates_format_of :phone_number, with: /\d[0-9]\)*\z/, message: I18n.t(:can_only_be_numerical)
   validates :phone_number, length: { minimum: 10, maximum: 15, message: I18n.t(:length_phone) }
 end
